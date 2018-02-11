adb918cc562caa41d7da27a573f11711deb7badc
Daniel Wilkins <danielwilkins8@gmail.com>
2018-02-10 19:05:42 -0700

    yew is now compiling

--- app/src/main.rs
+++ app/src/main.rs
@@ -2,19 +2,21 @@
 #[macro_use] extern crate yew;
 extern crate transfer;
 extern crate serde;
+extern crate serde_json;
 extern crate http;
 
-use yew::html::*;
+use yew::prelude::*;
+use yew::format::{ Json};
 use yew::services::console::ConsoleService;
-use yew::services::fetch::{FetchService, FetchHandle};
-use http::{Request, Response}
+use yew::services::fetch::{FetchService, FetchTask};
+use http::{Request, Response};
 
 
 use transfer::Creds;
 
 struct Context {
     console: ConsoleService,
-    web: FetchService
+    web: FetchService,
 }
 
 struct Model {
@@ -22,94 +24,123 @@ struct Model {
     password: String,
     login_attempts: i32,
     login_status: bool,
+    task: Option<FetchTask>,
 }
 
 enum Msg{
     UpdateEmail(String),
     UpdatePassword(String),
     SubmitLogin,
-    LoginFailed,
-    LoginSuccess,
+    LoginReady(Result<String, ()>),
 }
 
-fn update(context: &mut Context, model: &mut Model, msg: Msg) {
-    match msg {
-        Msg::UpdateEmail(val) => {
-            model.email = val;
-        },
-        Msg::UpdatePassword(val) => {
-            model.password = val;
-        },
-        Msg::SubmitLogin => {          
-            let creds = Creds { 
-                email: model.email,
-                password: model.password,
-            };
-            let login_request = Request::post("/api/login")
-                .header("Content-Type", "application/json")
-                .body(serde_json::to_string(&creds).unwrap());
+impl Component<Context> for Model {
+    type Msg = Msg;
+    type Properties = ();
 
-            context.web.fetch(
-                login_request,
-                |response| {
-                    if response.status().is_success() {
-                        Msg::LoginSuccess
-                    } else {
-                        Msg::LoginFailed
-                    }
-                }
-            )
+    fn create(_: Self::Properties, _: &mut Env<Context, Self>) -> Self {
+        Model {
+            email: "".to_string(),
+            password: "".to_string(),
+            login_attempts: 0,
+            login_status: false,
+            task: None,
         }
+    }
+
+    fn update(&mut self, msg: Self::Msg, context: &mut Env<Context, Self>) -> ShouldRender{
+        match msg {
+            Msg::UpdateEmail(val) => {
+                self.email = val;
+            },
+            Msg::UpdatePassword(val) => {
+                self.password = val;
+            },
+            Msg::SubmitLogin => {          
+                let creds = Creds { 
+                    email: self.email.to_string(),
+                    password: self.password.to_string(),
+                };
+                let login_request = Request::post("/api/login")
+                    .header("Content-Type", "application/json")
+                    .body(serde_json::to_string(&creds).unwrap()).unwrap();
+                
+                let callback = context.send_back(Msg::LoginReady);
+                let handler = move |response: Response<Json<Result<String, ()>>>| {
+                    let (_, Json(data)) = response.into_parts();
+                    callback.emit(data)
+                };
+
+                let task = context.web.fetch(
+                    login_request,
+                    handler.into()
+                );
+
+                self.task = Some(task);
+            },
+            Msg::LoginReady(Ok(status)) => {
+                let log = format!("Login Status: {}", status);
+                context.console.log(&log);
+                self.login_attempts += 1;
+                self.login_status = status == "true".to_string();
+            },
+            Msg::LoginReady(Err(_)) => {
+                context.console.log("Improper response from log in.");
+                self.login_attempts += 1;
+            }
+        }
+        true
     }
 }
 
-fn view(model: &Model) -> Html<Msg> {
-    html! {
-        <div>
-            <h1>{"Login"}</h1>
-        </div>
-        <div>{ 
-            if model.login_attempts > 0 && model.login_status == false { 
-                "Login Failed" 
-            } else {
-                ""
-            }
-        }</div>
-        <div>
-            <label>{ "Email" }</label>
-            <input type="text", 
-                id="email",
-                value=&model.email,
-                oninput=|e: InputData| Msg::UpdateEmail(e.value),>
-            </input>
-            <label>{ "Password" }</label>
-            <input type="password", 
-                id="password",
-                value=&model.password,
-                oninput=|e: InputData| Msg::UpdatePassword(e.value),>
-            </input>
-            <button type="submit",
-                id="submit",
-                onclick=move |_| Msg::SubmitLogin,>
-                {"Login"}
-            </button>
-        </div>
+
+impl Renderable<Context, Model> for Model {
+    fn view(&self) -> Html<Context, Self> {
+        html! {
+            <div>
+                <h1>{"Login"}</h1>
+            </div>
+            <div>{ 
+                if self.login_attempts > 0 && self.login_status == false { 
+                    "Login Failed" 
+                } else {
+                    ""
+                }
+            }</div>
+            <div>
+                <label>{ "Email" }</label>
+                <input type="text", 
+                    id="email",
+                    value=&self.email,
+                    oninput=|e: InputData| Msg::UpdateEmail(e.value),>
+                </input>
+                <label>{ "Password" }</label>
+                <input type="password", 
+                    id="password",
+                    value=&self.password,
+                    oninput=|e: InputData| Msg::UpdatePassword(e.value),>
+                </input>
+                <button type="submit",
+                    id="submit",
+                    onclick=move |_| Msg::SubmitLogin,>
+                    {"Login"}
+                </button>
+            </div>
+            <div>
+                {"Attempts: "}{ &self.login_attempts }
+            </div>
+        }
     }
 }
 
 fn main() {
     yew::initialize();
-    let mut app = App::new();
     let context = Context {
         console: ConsoleService,
         web: FetchService::new(),
     };
-    let model = Model {
-        email: "".to_string(),
-        password: "".to_string(),
-        login_attempts: 0,
-        login_status: false,
-    };
-    app.mount(context, model, update, view);
+    
+    let app: App<_, Model> = App::new(context);
+    app.mount_to_body();
     yew::run_loop();
 }
